b381e7148527b04aeb63035862b985682ba2269f
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-02-23 13:08:39 -0800

    Add register & login to ApplicationUser

---
+++ ParkLookup/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkLookup.ViewModels
+{
+  public class RegisterViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "UserName")]
+    public string UserName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
